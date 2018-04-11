ac8060882ab589554a908585e6e375b1113e627b
barndogghxc <barndogghxc@yahoo.com>
2018-04-11 04:30:39 -0400

    users being saved in firebase

--- Bookworm/screens/Login.js
+++ Bookworm/screens/Login.js
@@ -1,32 +1,56 @@
-   import React from 'react';
+import React from 'react';
 import style from '../style';
-import { View, Text, Alert } from 'react-native';
 import RootNavigator from '../nav/RootNavigator';
 import { connect } from 'react-redux';
-import { login } from '../redux/actions'
+import { login } from '../redux/actions';
+import * as firebase from 'firebase';
+import firebaseConfig from '../config/firebase.js';
+firebase.initializeApp(firebaseConfig);
+import { View, Text, Alert, TouchableOpacity } from 'react-native';
 
 
 class Login extends React.Component {
 	state = {}
-	
-  componentWillMount(){}
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user != null) {
+        this.props.dispatch(login(true))
+        console.log("Authenticated" + JSON.stringify(user));
+      }
+    });
+  }
 
 	login = async () => {
-    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('1073004776175191', {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('815516641982277', {
         permissions: ['public_profile'],
     	});
     if (type === 'success') {
+      // Build Firebase credential with the Facebook access token.
+      const credential = await firebase.auth.FacebookAuthProvider.credential(token);
 
+      // Sign in with credential from the Facebook user.
+      firebase.auth().signInWithCredential(credential).catch((error) => {
+        // Handle Errors here.
+        Alert.alert("Try Again")
+      });
     }
   } 
  
 	render() {
-    return (
-	   <View style={style.container}>
-		   <Text>Login</Text>
-	   </View>
-	  )
-	}
+    if(this.props.loggedIn){
+      return (
+        <RootNavigator/>
+      )
+    } else {
+      return (
+  	    <View style={style.container}>
+          <TouchableOpacity onPress={this.login.bind(this)}>
+            <Text>Login</Text>
+          </TouchableOpacity>
+  	    </View>
+	    )
+	  }
+  }
 }
 
 function mapStateToProps(state) {
--- Bookworm/style.js
+++ Bookworm/style.js
@@ -4,7 +4,7 @@ import { StyleSheet } from 'react-native';
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff69b4',
+    backgroundColor: '#d3d3d3',
     alignItems: 'center',
     justifyContent: 'center',
   },
