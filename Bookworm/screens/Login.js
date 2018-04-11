af850fa82a4d19c79a0980b041f1e3f82dd3a409
barndogghxc <barndogghxc@yahoo.com>
2018-04-11 03:14:52 -0400

    views working

--- Bookworm/screens/Login.js
+++ Bookworm/screens/Login.js
@@ -1,37 +1,29 @@
-import React from 'react';
+   import React from 'react';
 import style from '../style';
 import { View, Text, Alert } from 'react-native';
-import RootNavigator from './nav/RootNavigator';
+import RootNavigator from '../nav/RootNavigator';
 import { connect } from 'react-redux';
 import { login } from '../redux/actions'
-import * as firebase from 'firebase';
-import firebaseConfig from '../config/firebase.js'
-firebase.initializeApp(firebaseConfig);
+
 
 class Login extends React.Component {
 	state = {}
-	componentWillMount(){}
+	
+  componentWillMount(){}
 
 	login = async () => {
-      const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('1073004776175191', {
-          permissions: ['public_profile'],
-      	});
-      if (type === 'success') {
-      	// Build Firebase credential with the Facebook access token.
-      	const credential = await firebase.auth.FacebookAuthProvider.credential(token);
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('1073004776175191', {
+        permissions: ['public_profile'],
+    	});
+    if (type === 'success') {
 
-      	// Sign in with credential from the Facebook user.
-      	firebase.auth().signInWithCredential(credential).catch((error) => {
-        	// Handle Errors here.
-        	Alert.alert("Try Again")
-        });
-      }
-    } 
-
+    }
+  } 
+ 
 	render() {
-      return (
-	   <View>
-		 <Text>Login</Text>
+    return (
+	   <View style={style.container}>
+		   <Text>Login</Text>
 	   </View>
 	  )
 	}
